9d6b13f7c3c22f4183d5491fa819ed0274d3b878
Julien Gautier <gautier.julien@gmail.com>
2023-03-09 19:20:58 +0100

    Rework cache so it works with flutter run

--- native/src/cached_client.rs
+++ native/src/cached_client.rs
@@ -8,12 +8,17 @@ impl CachedClient {
         cache_file_path: &str,
         make_request: F,
     ) -> String {
-        let html = std::fs::read_to_string(cache_file_path);
+        let cache_file_path = format!("{}/{}", crate::config::CACHE_PATH ,  cache_file_path);
+        let html = std::fs::read_to_string(&cache_file_path);
         match html {
-            Ok(f) => f,
+            Ok(f) => {
+                println!("Read request from cache {}", &cache_file_path);
+                f
+            },
             Err(_) => {
+                println!("No file name {} in the cache", &cache_file_path);
                 let resp = make_request(&self.http_client);
-                let write_res = std::fs::write(cache_file_path, &resp);
+                let write_res = std::fs::write(&cache_file_path, &resp);
                 write_res.expect(format!("Can't write to file {}", cache_file_path).as_str());
                 resp
             }
