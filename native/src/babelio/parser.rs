29a6e7a1b8a218f0179d15b3955f9e5ba567469c
Julien Gautier <gautier.julien@gmail.com>
2023-03-22 22:06:19 +0100

    Babelio handle authors with only last name

--- native/src/babelio/parser.rs
+++ native/src/babelio/parser.rs
@@ -1,5 +1,5 @@
 use crate::common::{html_select, BookMetaData};
-use itertools::{Itertools};
+use itertools::Itertools;
 
 #[derive(PartialEq, Debug)]
 pub enum BlurbRes {
@@ -60,6 +60,42 @@ pub fn extract_blurb(html: &str) -> Option<BlurbRes> {
     }
 }
 
+fn extract_author(author_scope: scraper::ElementRef) -> crate::common::Author {
+    let author_span = author_scope
+        .first_child()
+        .expect("author_scope shoud have a first child <a ...>")
+        .first_child()
+        .expect("author scope > a shoud have a first child <span ...>");
+    let mut children = author_span.children();
+    let first_element = children
+        .next()
+        .expect("author scope > a > span shoud have a first child");
+    let first_name;
+    let last_name_element;
+    if let Some(text) = first_element.value().as_text() {
+        first_name = text.trim().to_string();
+        last_name_element = children
+            .next()
+            .expect("author scope > a > span shoud have a second child which is the last name");
+    } else {
+        first_name = "".to_string();
+        last_name_element = first_element;
+    }
+
+    let last_name = last_name_element
+        .first_child()
+        .unwrap()
+        .value()
+        .as_text()
+        .expect("should be a text")
+        .trim()
+        .to_string();
+    crate::common::Author {
+        first_name,
+        last_name,
+    }
+}
+
 pub fn extract_title_author_keywords(html: &str) -> Option<BookMetaData> {
     let doc = scraper::Html::parse_document(html);
 
@@ -91,38 +127,7 @@ pub fn extract_title_author_keywords(html: &str) -> Option<BookMetaData> {
         html_select("[itemprop=\"author\"][itemscope][itemtype=\"https://schema.org/Person\"]");
     let r = book_scope.select(&binding);
 
-    let authors = r
-        .map(|author_scope| {
-            let author_span = author_scope
-                .first_child()
-                .expect("author_scope shoud have a first child <a ...>")
-                .first_child()
-                .expect("author scope > a shoud have a first child <span ...>");
-            let first_name = author_span
-                .first_child()
-                .expect("author scope > a > span shoud have a first child which is first name")
-                .value()
-                .as_text()
-                .expect("should be a text")
-                .trim()
-                .to_string();
-            let last_name = author_span
-                .children()
-                .nth(1)
-                .expect("author scope > a > span shoud have a second child which is the last name")
-                .first_child()
-                .unwrap()
-                .value()
-                .as_text()
-                .expect("should be a text")
-                .trim()
-                .to_string();
-            crate::common::Author {
-                first_name,
-                last_name,
-            }
-        })
-        .collect_vec();
+    let authors = r.map(extract_author).collect_vec();
 
     let keywords_scope = book_scope
         .select(&html_select("[class=\"tags\"]"))
@@ -197,6 +202,50 @@ mod tests {
                     "thriller religieux",
                     "littérature irlandaise",
                     "irlande"
+                ]
+                .map(|s| s.to_string())
+                .to_vec(),
+            })
+        );
+    }
+
+    #[test]
+    pub fn extract_title_author_keywords_from_file_9782253143321() {
+        let html = std::fs::read_to_string(
+            "src/babelio/test/9782253143321_le_livre_tibetain_des_morts.html",
+        )
+        .unwrap();
+        let title_author_keywords = extract_title_author_keywords(&html);
+        assert_eq!(
+            title_author_keywords,
+            Some(BookMetaData {
+                title: Some("Bardo-Thödol : Le livre tibétain des morts".to_string()),
+                authors: vec![crate::common::Author {
+                    first_name: "".to_string(),
+                    last_name: "Padmasambhava".to_string()
+                }],
+                blurb: None,
+                keywords: [
+                    "document",
+                    "classique",
+                    "histoire",
+                    "mystique",
+                    "zen",
+                    "mort",
+                    "croyances",
+                    "pensées philosophiques",
+                    "libération",
+                    "réincarnation",
+                    "Médiumnité",
+                    "religion",
+                    "spiritualité",
+                    "Bouddhistes",
+                    "bouddhisme tibétain",
+                    "vie après la mort",
+                    "bouddhisme",
+                    "voyage initiatique",
+                    "ésotérisme",
+                    "philosophie"
                 ]
                 .map(|s| s.to_string())
                 .to_vec(),
