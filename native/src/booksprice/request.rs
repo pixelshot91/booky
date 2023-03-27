da39846e8cf8b2bbe237cd3ecf8f082b5724561a
Julien Gautier <gautier.julien@gmail.com>
2023-03-27 22:37:36 +0200

    Add bookprices with basic example of Selenium

---
+++ native/src/booksprice/request.rs
@@ -0,0 +1,52 @@
+//! Requires chromedriver running on port 9515:
+//!
+//!     chromedriver --port=9515
+//!
+//! Run as follows:
+//!
+//!     cargo run --example tokio_async
+
+use thirtyfour::prelude::*;
+use tokio;
+
+#[tokio::main]
+async fn selenium_fn() -> color_eyre::Result<()> {
+    // The use of color_eyre gives much nicer error reports, including making
+    // it much easier to locate where the error occurred.
+    color_eyre::install()?;
+
+    let caps = DesiredCapabilities::chrome();
+    let driver = WebDriver::new("http://localhost:9515", caps).await?;
+    // Navigate to https://wikipedia.org.
+    driver.goto("https://wikipedia.org").await?;
+    let elem_form = driver.find(By::Id("search-form")).await?;
+
+    // Find element from element.
+    let elem_text = elem_form.find(By::Id("searchInput")).await?;
+
+    // Type in the search terms.
+    elem_text.send_keys("selenium").await?;
+
+    // Click the search button.
+    let elem_button = elem_form.find(By::Css("button[type='submit']")).await?;
+    elem_button.click().await?;
+
+    // Look for header to implicitly wait for the page to load.
+    driver.find(By::ClassName("firstHeading")).await?;
+    assert_eq!(driver.title().await?, "Selenium - Wikipedia");
+
+    // Always explicitly close the browser. There are no async destructors.
+    driver.quit().await?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_selenium() {
+        selenium_fn();
+    }
+}
