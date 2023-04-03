cee744c7cbe32e1c4a567d9fd6f5f6f9aba914ec
Julien Gautier <julien.gautier@mobile-devices.fr>
2023-04-03 20:41:47 +0200

    Extract selenium POC into useful function

--- native/src/booksprice/request.rs
+++ native/src/booksprice/request.rs
@@ -5,47 +5,55 @@
 use thirtyfour::prelude::*;
 use tokio;
 
-// mod selenium_common;
-
-// use crate::common;
-
 #[tokio::main]
-async fn selenium_fn() -> color_eyre::Result<()> {
-    // The use of color_eyre gives much nicer error reports, including making
-    // it much easier to locate where the error occurred.
-    color_eyre::install()?;
-
-    // thirtyfour::resolve!();
-    // crate::local_tester!();
-
+async fn extract_price_from_isbn(
+    isbn: &str,
+) -> Result<Vec<f64>, thirtyfour::prelude::WebDriverError> {
     let caps = DesiredCapabilities::chrome();
     let driver = WebDriver::new("http://localhost:9515", caps).await?;
-    driver.goto("https://www.booksprice.com/comparePrice.do?l=y&searchType=compare&inputData=9782266071529").await?;
-    let elem_form = driver.find(By::Id("search-form")).await?;
 
-    // Find element from element.
-    let elem_text = elem_form.find(By::Id("searchInput")).await?;
+    extract_price_from_url(
+        driver,
+        &format!(
+            "https://www.booksprice.com/comparePrice.do?l=y&searchType=compare&inputData={}",
+            isbn
+        ),
+    )
+    .await
+}
 
-    // Type in the search terms.
-    elem_text.send_keys("selenium").await?;
+async fn extract_price_from_url(c: WebDriver, url: &str) -> Result<Vec<f64>, WebDriverError> {
+    c.goto(&url).await?;
+    let entries = c
+        .find_all(By::XPath("//*[@id='chart']/tbody/tr[position()>1]"))
+        .await?;
+    assert_eq!(entries.len(), 6);
 
-    // Click the search button.
-    let elem_button = elem_form.find(By::Css("button[type='submit']")).await?;
-    elem_button.click().await?;
+    let prices = futures::future::try_join_all(entries.iter().map(|e| async {
+        let price_text = e
+            .find(By::XPath("td[@title='Total']/a/em"))
+            .await
+            .unwrap()
+            .text()
+            .await;
 
-    // Look for header to implicitly wait for the page to load.
-    driver.find(By::ClassName("firstHeading")).await?;
-    assert_eq!(driver.title().await?, "Selenium - Wikipedia");
+        price_text.map(|price_text| {
+            use regex::Regex;
+            let re = Regex::new(r"\$ (\d+\.?\d+)").unwrap();
+            let r = re.captures(&price_text).unwrap();
+            r.get(1).unwrap().as_str().parse::<f64>().unwrap()
+        })
+    }))
+    .await
+    .unwrap();
 
-    // Always explicitly close the browser. There are no async destructors.
-    driver.quit().await?;
+    c.close_window().await;
 
-    Ok(())
+    Ok(prices)
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::booksprice::selenium_common;
     use crate::booksprice::selenium_common::handle_test_error;
     use crate::booksprice::selenium_common::make_capabilities;
     use crate::booksprice::selenium_common::make_url;
@@ -55,40 +63,23 @@ mod tests {
 
     use super::*;
 
-    async fn parse_booksprices(c: WebDriver, port: u16) -> Result<(), WebDriverError> {
-        let url = selenium_common::url_from_path(port, "9782884747974.html");
+    async fn parse_booksprices_from_9782884747974(
+        c: WebDriver,
+        port: u16,
+    ) -> Result<(), WebDriverError> {
+        use crate::booksprice::selenium_common;
 
-        c.goto(&url).await?;
-        let entries = c
-            .find_all(By::XPath("//*[@id='chart']/tbody/tr[position()>1]"))
-            .await?;
-        assert_eq!(entries.len(), 6);
+        let prices = extract_price_from_url(
+            c,
+            &selenium_common::url_from_path(port, "9782884747974.html"),
+        ).await.unwrap();
 
-        let prices = futures::future::try_join_all(entries.iter().map(|e| async {
-            let price_text = e
-                .find(By::XPath("td[@title='Total']/a/em"))
-                .await
-                .unwrap()
-                .text()
-                .await;
-
-            price_text.map(|price_text| {
-                use regex::Regex;
-                let re = Regex::new(r"\$ (\d+\.?\d+)").unwrap();
-                let r = re.captures(&price_text).unwrap();
-                r.get(1).unwrap().as_str().parse::<f64>().unwrap()
-            })
-        }))
-        .await
-        .unwrap();
-
-        assert_eq!(prices, vec![16.55, 21.85, 23.75, 27.17, 28.15, 43.20]);
-
-        c.close_window().await
+        assert_eq!(prices, vec![16.55,21.85,23.75,27.17,28.15,43.20]);
+        Ok(())
     }
 
     #[test]
     fn test_selenium() {
-        local_tester!(parse_booksprices, "chrome");
+        local_tester!(parse_booksprices_from_9782884747974, "chrome");
     }
 }
