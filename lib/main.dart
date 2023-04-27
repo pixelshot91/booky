08a0e307ff842edbf5cdfcc973e525741c1caad0
Julien Gautier <julien.gautier@mobile-devices.fr>
2023-04-28 00:02:24 +0200

    Merge Camera_app. Compile on Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,36 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_rust_bridge_template/camera/camera.dart';
 
 import 'enrichment/enrichment.dart';
 
 void main() {
-  runApp(const EnrichmentApp());
+  runApp(const BookyApp());
+}
+
+enum BookyAppActivity {
+  camera,
+  enrichment,
+}
+
+class BookyApp extends StatefulWidget {
+  const BookyApp();
+
+  @override
+  State<BookyApp> createState() => _BookyAppState();
+}
+
+class _BookyAppState extends State<BookyApp> {
+  BookyAppActivity activity = Platform.isAndroid ? BookyAppActivity.camera : BookyAppActivity.enrichment;
+
+  @override
+  Widget build(BuildContext context) {
+    switch (activity) {
+      case BookyAppActivity.camera:
+        return const CameraApp();
+      case BookyAppActivity.enrichment:
+        return const EnrichmentApp();
+    }
+  }
 }
