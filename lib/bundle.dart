7e7a7f247814c7c14bb70173d3c371e85b00877e
Julien Gautier <julien.gautier@mobile-devices.fr>
2023-05-03 22:59:18 +0200

    ISBNDecoding: Sort image by name

--- lib/bundle.dart
+++ lib/bundle.dart
@@ -10,22 +10,23 @@ class Bundle {
 
   final Directory directory;
 
-  Iterable<File> get images {
-    return directory.listSync().whereType<File>().where((file) => path.extension(file.path) == '.jpg');
-  }
+  Iterable<File> get images => directory.listImages();
 
   Directory get compressedImagesDir => Directory(path.join(directory.path, 'compressed'));
 
-  Iterable<File> get compressedImages {
-    return compressedImagesDir
-        .listSync()
-        .whereType<File>()
-        .where((file) => path.extension(file.path) == '.jpg')
-        .sorted((f1, f2) => path.basename(f1.path).compareTo(path.basename(f2.path)));
-  }
+  Iterable<File> get compressedImages => compressedImagesDir.listImages();
 
   Metadata get metadata {
     final metadataFile = File(path.join(directory.path, 'metadata.json'));
     return Metadata.fromJson(jsonDecode(metadataFile.readAsStringSync()) as Map<String, dynamic>);
   }
+}
+
+extension _DirExt on Directory {
+  Iterable<File> listImages() =>
+      listSync().whereType<File>().where((file) => path.extension(file.path) == '.jpg').sortByName();
+}
+
+extension _ListFileExt on Iterable<File> {
+  List<File> sortByName() => sorted((f1, f2) => path.basename(f1.path).compareTo(path.basename(f2.path)));
 }
