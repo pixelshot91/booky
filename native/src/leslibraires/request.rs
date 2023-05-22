a63ee1f83cbd94e516a97cad1bf5243b1c13f98b
Julien Gautier <julien.gautier@mobile-devices.fr>
2023-05-23 00:22:45 +0200

    Fix LesLibraires providers

--- native/src/leslibraires/request.rs
+++ native/src/leslibraires/request.rs
@@ -5,27 +5,23 @@ pub fn isbn_search(client: &CachedClient, isbn: &str) -> String {
         format!("leslibraires/isbn_{}.html", isbn).as_str(),
         &|http_client| {
             http_client
-                .get(format!(
-                    "https://www.leslibraires.fr/recherche/?q={}",
-                    &isbn
-                ))
+                .get(format!("https://www.leslibraires.fr/article/{}", &isbn))
                 .send()
                 .unwrap()
-                .headers()
-                .get("location")
-                .map(|header_value| header_value.to_str().unwrap())
-                .unwrap()
+                .url()
+                .path()
                 .to_owned()
         },
     );
     let result = client.make_request(
-        format!("leslibraires/get_book_url_{}.html", redirection).as_str(),
+        format!(
+            "leslibraires/get_book_url_{}.html",
+            redirection.replace("/", "_slash_")
+        )
+        .as_str(),
         &|http_client| {
             http_client
-                .get(format!(
-                    "https://www.leslibraires.fr/{}",
-                    &redirection
-                ))
+                .get(format!("https://www.leslibraires.fr{}", &redirection))
                 .send()
                 .unwrap()
                 .text()
@@ -35,3 +31,17 @@ pub fn isbn_search(client: &CachedClient, isbn: &str) -> String {
     result
 }
 
+/* #[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_isbn_search() {
+        let client = reqwest::blocking::Client::builder().build().unwrap();
+        let cached_client = CachedClient {
+            http_client: client,
+        };
+        isbn_search(&cached_client, "9782286056636");
+    }
+}
+ */
