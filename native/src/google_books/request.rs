0d2abe40a317ccdd41d2fb9f549c55cc91268170
Julien Gautier <gautier.julien@gmail.com>
2023-06-24 15:01:44 +0200

    Rust: GoogleBooks: move cache files into goole_books/

--- native/src/google_books/request.rs
+++ native/src/google_books/request.rs
@@ -1,7 +1,9 @@
 use crate::cached_client::Client;
 
+const CACHE_DIR: &str = "google_books";
+
 pub fn search_by_isbn(client: &Box<dyn Client>, isbn: &str) -> String {
-    client.make_request_as_text(&format!("search_by_isbn_{}", isbn), &|client| {
+    client.make_request_as_text(&format!("{}/search_by_isbn_{}", CACHE_DIR, isbn), &|client| {
         client
             .get(format!(
                 "https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}"
@@ -11,7 +13,7 @@ pub fn search_by_isbn(client: &Box<dyn Client>, isbn: &str) -> String {
 }
 pub fn get_volume(client: &Box<dyn Client>, url: &str) -> String {
     client.make_request_as_text(
-        &format!("get_volume_{}", crate::common::url_to_path(url)),
+        &format!("{}/get_volume_{}", CACHE_DIR, crate::common::url_to_path(url)),
         &|http_client| http_client.get(url).send(),
     )
 }
