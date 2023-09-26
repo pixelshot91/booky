37a414bc65561a45a6c68d3469fa379948b9ecc4
Julien Gautier <gautier.julien@gmail.com>
2023-09-26 20:36:38 +0200

    Rust: add get/set_manual_md, get_merged_md, add config file

--- native/src/bridge_generated.io.rs
+++ native/src/bridge_generated.io.rs
@@ -21,21 +21,37 @@ pub extern "C" fn wire_get_auto_metadata_from_bundle(port_: i64, path: *mut wire
 }
 
 #[no_mangle]
+pub extern "C" fn wire_get_manual_metadata_for_bundle(
+    port_: i64,
+    bundle_path: *mut wire_uint_8_list,
+) {
+    wire_get_manual_metadata_for_bundle_impl(port_, bundle_path)
+}
+
+#[no_mangle]
+pub extern "C" fn wire_set_manual_metadata_for_bundle(
+    port_: i64,
+    bundle_path: *mut wire_uint_8_list,
+    bundle_metadata: *mut wire_BundleMetaData,
+) {
+    wire_set_manual_metadata_for_bundle_impl(port_, bundle_path, bundle_metadata)
+}
+
+#[no_mangle]
+pub extern "C" fn wire_get_merged_metadata_for_bundle(
+    port_: i64,
+    bundle_path: *mut wire_uint_8_list,
+) {
+    wire_get_merged_metadata_for_bundle_impl(port_, bundle_path)
+}
+
+#[no_mangle]
 pub extern "C" fn wire_get_metadata_from_provider(
     port_: i64,
     provider: i32,
     isbn: *mut wire_uint_8_list,
 ) {
     wire_get_metadata_from_provider_impl(port_, provider, isbn)
-}
-
-#[no_mangle]
-pub extern "C" fn wire_publish_ad(
-    port_: i64,
-    ad: *mut wire_Ad,
-    credential: *mut wire_LbcCredential,
-) {
-    wire_publish_ad_impl(port_, ad, credential)
 }
 
 // Section: allocate functions
@@ -50,13 +66,36 @@ pub extern "C" fn new_StringList_0(len: i32) -> *mut wire_StringList {
 }
 
 #[no_mangle]
-pub extern "C" fn new_box_autoadd_ad_0() -> *mut wire_Ad {
-    support::new_leak_box_ptr(wire_Ad::new_with_null_ptr())
+pub extern "C" fn new_box_autoadd_bundle_meta_data_0() -> *mut wire_BundleMetaData {
+    support::new_leak_box_ptr(wire_BundleMetaData::new_with_null_ptr())
 }
 
 #[no_mangle]
-pub extern "C" fn new_box_autoadd_lbc_credential_0() -> *mut wire_LbcCredential {
-    support::new_leak_box_ptr(wire_LbcCredential::new_with_null_ptr())
+pub extern "C" fn new_box_autoadd_i32_0(value: i32) -> *mut i32 {
+    support::new_leak_box_ptr(value)
+}
+
+#[no_mangle]
+pub extern "C" fn new_box_autoadd_item_state_0(value: i32) -> *mut i32 {
+    support::new_leak_box_ptr(value)
+}
+
+#[no_mangle]
+pub extern "C" fn new_list_author_0(len: i32) -> *mut wire_list_author {
+    let wrap = wire_list_author {
+        ptr: support::new_leak_vec_ptr(<wire_Author>::new_with_null_ptr(), len),
+        len,
+    };
+    support::new_leak_box_ptr(wrap)
+}
+
+#[no_mangle]
+pub extern "C" fn new_list_book_meta_data_0(len: i32) -> *mut wire_list_book_meta_data {
+    let wrap = wire_list_book_meta_data {
+        ptr: support::new_leak_vec_ptr(<wire_BookMetaData>::new_with_null_ptr(), len),
+        len,
+    };
+    support::new_leak_box_ptr(wrap)
 }
 
 #[no_mangle]
@@ -87,36 +126,69 @@ impl Wire2Api<Vec<String>> for *mut wire_StringList {
         vec.into_iter().map(Wire2Api::wire2api).collect()
     }
 }
-impl Wire2Api<Ad> for wire_Ad {
-    fn wire2api(self) -> Ad {
-        Ad {
-            title: self.title.wire2api(),
-            description: self.description.wire2api(),
-            price_cent: self.price_cent.wire2api(),
-            weight_grams: self.weight_grams.wire2api(),
-            imgs_path: self.imgs_path.wire2api(),
+impl Wire2Api<Author> for wire_Author {
+    fn wire2api(self) -> Author {
+        Author {
+            first_name: self.first_name.wire2api(),
+            last_name: self.last_name.wire2api(),
         }
     }
 }
-impl Wire2Api<Ad> for *mut wire_Ad {
-    fn wire2api(self) -> Ad {
-        let wrap = unsafe { support::box_from_leak_ptr(self) };
-        Wire2Api::<Ad>::wire2api(*wrap).into()
+impl Wire2Api<BookMetaData> for wire_BookMetaData {
+    fn wire2api(self) -> BookMetaData {
+        BookMetaData {
+            isbn: self.isbn.wire2api(),
+            title: self.title.wire2api(),
+            authors: self.authors.wire2api(),
+            blurb: self.blurb.wire2api(),
+            keywords: self.keywords.wire2api(),
+            price_cent: self.price_cent.wire2api(),
+        }
     }
 }
-impl Wire2Api<LbcCredential> for *mut wire_LbcCredential {
-    fn wire2api(self) -> LbcCredential {
+impl Wire2Api<BundleMetaData> for *mut wire_BundleMetaData {
+    fn wire2api(self) -> BundleMetaData {
         let wrap = unsafe { support::box_from_leak_ptr(self) };
-        Wire2Api::<LbcCredential>::wire2api(*wrap).into()
+        Wire2Api::<BundleMetaData>::wire2api(*wrap).into()
+    }
+}
+impl Wire2Api<i32> for *mut i32 {
+    fn wire2api(self) -> i32 {
+        unsafe { *support::box_from_leak_ptr(self) }
+    }
+}
+impl Wire2Api<ItemState> for *mut i32 {
+    fn wire2api(self) -> ItemState {
+        let wrap = unsafe { support::box_from_leak_ptr(self) };
+        Wire2Api::<ItemState>::wire2api(*wrap).into()
+    }
+}
+impl Wire2Api<BundleMetaData> for wire_BundleMetaData {
+    fn wire2api(self) -> BundleMetaData {
+        BundleMetaData {
+            weight_grams: self.weight_grams.wire2api(),
+            item_state: self.item_state.wire2api(),
+            books: self.books.wire2api(),
+        }
     }
 }
 
-impl Wire2Api<LbcCredential> for wire_LbcCredential {
-    fn wire2api(self) -> LbcCredential {
-        LbcCredential {
-            lbc_token: self.lbc_token.wire2api(),
-            datadome_cookie: self.datadome_cookie.wire2api(),
-        }
+impl Wire2Api<Vec<Author>> for *mut wire_list_author {
+    fn wire2api(self) -> Vec<Author> {
+        let vec = unsafe {
+            let wrap = support::box_from_leak_ptr(self);
+            support::vec_from_leak_ptr(wrap.ptr, wrap.len)
+        };
+        vec.into_iter().map(Wire2Api::wire2api).collect()
+    }
+}
+impl Wire2Api<Vec<BookMetaData>> for *mut wire_list_book_meta_data {
+    fn wire2api(self) -> Vec<BookMetaData> {
+        let vec = unsafe {
+            let wrap = support::box_from_leak_ptr(self);
+            support::vec_from_leak_ptr(wrap.ptr, wrap.len)
+        };
+        vec.into_iter().map(Wire2Api::wire2api).collect()
     }
 }
 
@@ -139,19 +211,42 @@ pub struct wire_StringList {
 
 #[repr(C)]
 #[derive(Clone)]
-pub struct wire_Ad {
-    title: *mut wire_uint_8_list,
-    description: *mut wire_uint_8_list,
-    price_cent: i32,
-    weight_grams: i32,
-    imgs_path: *mut wire_StringList,
+pub struct wire_Author {
+    first_name: *mut wire_uint_8_list,
+    last_name: *mut wire_uint_8_list,
 }
 
 #[repr(C)]
 #[derive(Clone)]
-pub struct wire_LbcCredential {
-    lbc_token: *mut wire_uint_8_list,
-    datadome_cookie: *mut wire_uint_8_list,
+pub struct wire_BookMetaData {
+    isbn: *mut wire_uint_8_list,
+    title: *mut wire_uint_8_list,
+    authors: *mut wire_list_author,
+    blurb: *mut wire_uint_8_list,
+    keywords: *mut wire_StringList,
+    price_cent: *mut i32,
+}
+
+#[repr(C)]
+#[derive(Clone)]
+pub struct wire_BundleMetaData {
+    weight_grams: *mut i32,
+    item_state: *mut i32,
+    books: *mut wire_list_book_meta_data,
+}
+
+#[repr(C)]
+#[derive(Clone)]
+pub struct wire_list_author {
+    ptr: *mut wire_Author,
+    len: i32,
+}
+
+#[repr(C)]
+#[derive(Clone)]
+pub struct wire_list_book_meta_data {
+    ptr: *mut wire_BookMetaData,
+    len: i32,
 }
 
 #[repr(C)]
@@ -173,34 +268,51 @@ impl<T> NewWithNullPtr for *mut T {
     }
 }
 
-impl NewWithNullPtr for wire_Ad {
+impl NewWithNullPtr for wire_Author {
     fn new_with_null_ptr() -> Self {
         Self {
-            title: core::ptr::null_mut(),
-            description: core::ptr::null_mut(),
-            price_cent: Default::default(),
-            weight_grams: Default::default(),
-            imgs_path: core::ptr::null_mut(),
+            first_name: core::ptr::null_mut(),
+            last_name: core::ptr::null_mut(),
         }
     }
 }
 
-impl Default for wire_Ad {
+impl Default for wire_Author {
     fn default() -> Self {
         Self::new_with_null_ptr()
     }
 }
 
-impl NewWithNullPtr for wire_LbcCredential {
+impl NewWithNullPtr for wire_BookMetaData {
     fn new_with_null_ptr() -> Self {
         Self {
-            lbc_token: core::ptr::null_mut(),
-            datadome_cookie: core::ptr::null_mut(),
+            isbn: core::ptr::null_mut(),
+            title: core::ptr::null_mut(),
+            authors: core::ptr::null_mut(),
+            blurb: core::ptr::null_mut(),
+            keywords: core::ptr::null_mut(),
+            price_cent: core::ptr::null_mut(),
         }
     }
 }
 
-impl Default for wire_LbcCredential {
+impl Default for wire_BookMetaData {
+    fn default() -> Self {
+        Self::new_with_null_ptr()
+    }
+}
+
+impl NewWithNullPtr for wire_BundleMetaData {
+    fn new_with_null_ptr() -> Self {
+        Self {
+            weight_grams: core::ptr::null_mut(),
+            item_state: core::ptr::null_mut(),
+            books: core::ptr::null_mut(),
+        }
+    }
+}
+
+impl Default for wire_BundleMetaData {
     fn default() -> Self {
         Self::new_with_null_ptr()
     }
---
+++ native/src/config.rs
@@ -0,0 +1,1 @@
+pub const CACHE_PATH: &str = "native/cache";
