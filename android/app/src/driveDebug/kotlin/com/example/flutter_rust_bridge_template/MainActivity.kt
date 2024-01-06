11ed0ef39902361270574808882530c8b7474f7f
Julien Gautier <gautier.julien@gmail.com>
2024-01-06 14:24:01 +0100

    Run on linux, but Android crash at startup because it cannot find MainActivity

--- android/app/src/driveDebug/kotlin/com/example/flutter_rust_bridge_template/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package fr.pimoid.booky
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/common.dart
+++ lib/common.dart
@@ -56,8 +56,8 @@ extension DirectoryExt on Directory {
   File joinFile(String f) => File(path.join(this.path, f));
 }
 
-final externalDeviceRepo = Directory('/media/phone/storage/emulated/0/Android/data/fr.pimoid.booky/files');
-// final externalDeviceRepo = Directory('/home/julien/Perso/LeBonCoin/chain_automatisation/booky/extra/mock_data/basic/');
+// final externalDeviceRepo = Directory('/media/phone/storage/emulated/0/Android/data/fr.pimoid.booky/files');
+final externalDeviceRepo = Directory('/home/julien/Perso/LeBonCoin/chain_automatisation/booky/extra/mock_data/basic/');
 
 extension ItemStateExt on rust.ItemState {
   String get loc {
